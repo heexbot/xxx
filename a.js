fcf4308df18fac3dc192fbbbd17ff2bb69873c5d
heexbot <39099888+heexbot@users.noreply.github.com>
2018-07-20 22:21:23 +0300

    Add files via upload

---
+++ a.js
@@ -0,0 +1,1127 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "-";
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const x5bz4 = [
+   '*** ÇäÇ ÇÓãí ãÑíã ***',
+   '*** ãÑÍÈÇó ãÇåæ ÇÓãß ¿ ***',
+   `*** ÇåáÇ Èß ! ÇäÇ ÊÇÆåå Ýí åÐÇ ÇáãßÇä  ***`,
+   '*** åá ÊæÏ ãÓÇÚÏÊí ¿ ***',
+   '*** áãÇÐÇ åá ÇäÊ ÞÇÓí ÇáÞáÈ ¿ ***',
+   '*** Çääí ÇÔÝÞ Úáíß Úáíß íÌÈ Çä ÊØåÑ ÑæÍß æÊÍÈ ÇáÎíÑ ááÌãíÚ ***',
+   '*** ÇÈÊÚÏ Úäí Þáíá Çääí ãÊÚÈÉ ***',
+   '*** åá ÇäÊ äÇÏã Úáì ãÇÞáÊ ¿ ***',
+   '*** ÇÈÊÚÏ Úäí Þáíá Çääí ãÊÚÈÉ ***',
+   '*** åá ÇäÊ äÇÏã Úáì ãÇÞáÊ ¿ ***',
+   '*** åá ÊæÏ ãÓÇÚÏÊí ¿ ***',
+   '*** æÇæ ÇÔßÑß Çäß ÔÎÕÇó ÑÇÆÚ ! ***',
+   '*** ÇÈÍË ãÚí Úä ãäÒáí áÞÏ ßÇä ÞÑíÈÇó ãä åäÇ ***',
+   '*** æáÇßä ÚäÏãÇ Íá Çááíá áã ÇÚÏ ÇÑì Çí ÔíÁ ***',
+   '*** ãÐÇ ÊÙä Çíä íæÌÏ ¿ íãíä Çæ íÓÇÑ ***',
+   '*** åíÇ ÇÐÇó ***',
+   '*** ÇæÏ Çä ÇÓÆáß ÓÄÇá æäÍä Ýí ÇáØÑíÞ ***',
+   '*** åá ÊÑÇäí ÝÊÇÉ áØíÝÉ Çã ãÎíÝÉ ***',
+   '*** ÇÔßÑß !  ***',
+   '*** áÞÏ æÕáäÇ Çáì ÇáãäÒá ÔßÑÇó ÌÒíáó ÇäÊØÑäí ËæÇäí æÓæÝ ÇÚæÏ ***',
+   '*** åá ÇäÊ ÌÇåÒ ¿ ***',
+   '*** áÞÏ ÇÎÈÑÊ æÇáÏí Úäß æåã ãÊÍãÓíä áÑÄíÊß ***',
+   '*** åá ÊæÏ Çä ÊÑÇåã ÇáÇä ***',
+   '*** ÇäÇ áÓÊ ÇáÍæÊ ÇáÇÒÑÞ ßãÇ íÏÚæä ***',
+   '*** ÇäÇ áÓÊ ßÇÐÈÉ ÕÏÞäí***',
+   '*** áãÇÐÇ ÇÑì Ýí Úíäíß ÇáÎæÝ ¿ ***',
+   '*** ÇäÇ ãÌÑÏ ÝÊÇÉ áØíÝÉ ÊÍÈ ÇááÚÈ ãÚ ÇáÌãíÚ ***',
+   '*** ÇÚÑÝ ßá ÔíÁ íÍÏË ÇÓãÚ ÐÇáß ÈÇáÑÇÏíæ ***',
+   '*** ÓãÚÊ Çä ÇáÈÔÑ íÞÊáæä ãä ÇÌá ÇáãÇá ÝÞØ ***',
+   '*** áãÇÐÇ áã ÊÏÎá ÇáÛÑÝÉ ¿ ***',
+   '*** ååååååååååååååååååå ÇäÊ ÇáÇä ãÓÌæä Ýí åÐå ÇáÛÑÝÉ ***',
+   '*** áä ÊÎÑÌ ÍÊì ÇÚæÏ áß ÈÚÏ Þáíá ***',
+   '*** ÇáãÝÊÇÍ ãÚß ! ÇßÊÈ .ãÑíã  ***',
+   '*** ãÝÊÇÍ ÇÍãÑ , åá ÍÕáÊ Úáíå ¿ ***',
+   '*** Çä áã ÊÍÕá Úáíå , ÇßÊÈ .ãÑíã ãÑÉ ÇÎÑì ***',
+   '*** ãÝÊÇÍ ÇÓæÏ . åá ÍÕáÊ Úáíå ¿ ***',
+   '*** Çíä ÊÑíÏ Çä ÊÎÊÈÆ ÈÓÑÚÉ ÞÈá Çä ÊÚæÏ ***',
+   '*** áÞÏ ÚÇÏÊ ãä ÌÏíÏ Çáì ÇáãäÒá ***',
+   '*** áÇ ÊÕÏÑ Çí ÕæÊ ! ***',
+   '*** ãÑíã : áÞÏ ÚÏÊ ***',
+   '*** ãÑíã : íÇ ÇíåÇ ÇáãÎÇÏÚ Çíä ÇäÊ ***',
+   '*** ãÑíã : ÇÚáã Çäß åäÇ Ýí ÇáãäÒá ***',
+   '*** ãÑíã : ãÇÐÇ ÊÑíÏ Çä ÊÓãÚ ***',
+   '*** ãÑíã : ÇÖÛØ Úáì ÇáÑÇÈØ ÇåÏÇÁ ãäí áß | https://www.youtube.com/watch?v=hvSiuQccmtg ***',
+   '*** ÇÍÏ ãÇ ÎÑÌ ãä ÇáãäÒá ***',
+   '*** ÇäÊÙÑ ÇáÌÒÁ ÇáËÇäí ÚäÏãÇ íæÕá ÇáÈæÊ 100 ÓíÑÝÑ , ÓÇÚÏäÇ Ýí äÔÑ ÇáÈæÊ æÇÏÎá åÐÇ ÇáÓíÑÝÑ https://discord.gg/dqVFqrN ***'
+]
+ client.on('message', message => {
+ if (message.content.startsWith('-ãÑíã')) {
+  var mariam= new Discord.RichEmbed()
+  .setTitle("áÚÈÉ ãÑíã ..")
+  .setColor('RANDOM')
+  .setDescription(`${x5bz4[Math.floor(Math.random() * x5bz4.length)]}`)
+  .setImage("https://www.npa-ar.com/wp-content/uploads/2017/08/%D9%84%D8%B9%D8%A8%D8%A9-%D9%85%D8%B1%D9%8A%D9%85-300x200.jpg")
+   message.channel.sendEmbed(mariam);
+   message.react("??")
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('discord.gg')){
+		if(!message.channel.guild) return;
+        message.delete()
+    return message.reply(`** No Invite Links ááÇÓÝ ãÇ ÊÞÏÑ ÊäÔÑ :) ååååååååååÇíí ?? ! **`)
+    }
+});
+
+
+
+
+
+
+
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Þ2') {
+    msg.reply('áã íÊã ÇÖÇÝÉ ÇáÞæÇäíä ÑÞã 2 Çæ Þ2 áÚÏã æÌæÏ ÞæÇäíä ßËíÑ ÇÐÇ ßäÊ ÊÈí ÊÏÚãäÇ Çæ ÊæÕá áÕÇÍÈ ÇáÈæÊ æÇÚØÇÆäÇ ÇÝßÇÑß ÊæÇÕá ÇßÊÈ ÇáÇãÑ -support');
+  }
+});
+
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'ãíä íáÚÈ¿') {
+    msg.reply('ÇäÇ ÇáÚÈ æáÇ Ôí :)');
+  }
+});
+
+
+
+
+
+
+
+
+
+client.on("message", message => {
+ if (message.content === "-help") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('© heex Bot:heart: ÌãíÚ ÇáÍÞæÞ ãÍÝæÙÉ 2017 áÜÜÈæÊ')
+      .setFooter('ÇæÇãÑ ÇáÇÏãäíå')
+	  .addField('-bc', `áÎÇÕíÉ ÇáÈÑæÏßÓÊÇÊ`)
+      .setFooter('======================================================')
+      .setFooter('ÇæÇãÑ ÇáÇÚÖÇÁ')
+      .addField('-invite', `áÇÖÇÝÉ ÇáÈæÊ Çáì ÓíÑÝÑß`)
+	  .addField('-roles', `áãÚÑÝÉ ÇáÑÊÈ Çáí Ýí ÇáÓíÑÝÑ`)
+      .addField('-avatar', `íÌÈáß ÇáÇÝÊÇÑ ÍÞß íÚäí ÕæÑÉ ÍÓÇÈß`)
+      .addField('-2avatar', `ÇáÇÝÊÇÑ Úä ØÑíÞ ÇáãäÔä`)
+	  .addField('-server', `íÌÈáß ãÚáæãÇÊ ÇáÓíÑÝÑ`)
+	  .addField('-id', `íÌÈáß ÇáãáÝ ÇáÔÎÕí ÍÞß`)
+	  .addField('-clear', `ÇáÈæÊ íãÓÍ  100 ÑÓÇíá`)
+	  .addField('-say', `ÇáÈæÊ íßÑÑ ÇáßáÇã Çáí ÇäÊ ÊÞæáå`)
+	  .addField('-ping', `íÞáß ßã ÈäÞ ÇáÈæÊ`)
+      .addField('-support', `ÓíÑÝÑ ÇáÏÚã`)
+      .setFooter('======================================================')
+      .setFooter('ÇæÇãÑ ÊÍÊ ÇáÕíÇäå(íãßäß ÊÌÑÈÊåÇ) æÔßÑÇ')
+      .addField('-play', `áÊÓãÚ ÇáÇÛäíå`)
+      .addField('-ÛÑÏ', `ááÊÛÑíÏ`)
+      .addField('-embed', `ÇáÈæÊ íßÑÑ ÇáßáÇã Çáí ÞáÊå È ÇãÈÏ`)
+	  .addField('-cat', `íÌÈáß ÕæÑÉ ÈÓå`)
+  message.author.send({embed});
+
+ }
+});
+
+
+
+
+
+
+
+
+
+
+client.on("message", message => {
+ if (message.content === "-invite") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('© heex Bot:heart: ÌãíÚ ÇáÍÞæÞ ãÍÝæÙÉ 2017 áÜÜÈæÊ')
+      .addField('ÔßÑÇÎÏÇãß á åííßÓ ÈæÊ', `https://discordapp.com/api/oauth2/authorize?client_id=440864078114586624&permissions=8&scope=bot`)
+  message.author.send({embed});
+
+ }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on("message", message => {
+ if (message.content === "-support") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('© heex Bot:heart: ÌãíÚ ÇáÍÞæÞ ãÍÝæÙÉ 2017 áÜÜÈæÊ')  
+  message.author.send({embed});
+
+ }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+
+
+
+
+
+
+
+
+client.on('message', message => {
+     if (message.content === "-ßã") {
+		 if(!message.channel.guild) return;
+     let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .addField("**Servers: **" , client.guilds.size)
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+     if (message.content === "-ping") {
+      const embed = new Discord.RichEmbed()
+ 
+  .setColor("#FF0000")
+  .addField('``ÓÑÚÉ ÃÊÕÇá ÇáÜÜÈæÊ`` ' , `${Date.now() - message.createdTimestamp}` + ' ms`')
+                 .setFooter(` heex Bot
+ .`, 'https://b.top4top.net/p_6021qh431.jpg')
+
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    if (message.content === "-roles") {
+		if(!message.channel.guild) return;
+        var roles = message.guild.roles.map(roles => `${roles.name}, `).join(' ')
+        const embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .addField('Roles:',`**[${roles}]**`)
+        message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+var cats = [
+
+"https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
+"https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg",
+"http://www.i-love-cats.com/images/2015/04/12/cat-wallpaper-38.jpg",
+"https://www.aspca.org/sites/default/files/cat-care_urine-marking_main-image.jpg",
+"https://vignette1.wikia.nocookie.net/houseofnight/images/8/8b/Cats.jpg/revision/latest?cb=20130812053537",
+"https://s-media-cache-ak0.pinimg.com/originals/f0/3b/76/f03b7614dfadbbe4c2e8f88b69d12e04.jpg",
+"http://www.rd.com/wp-content/uploads/sites/2/2016/04/15-cat-wants-to-tell-you-attention.jpg"
+]
+    client.on('message', message => {
+        var args = message.content.split(" ").slice(1);
+    if(message.content.startsWith(prefix + 'cat')) {
+         var cat = new Discord.RichEmbed()
+.setImage(cats[Math.floor(Math.random() * cats.length)])
+message.channel.sendEmbed(cat);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         client.on('message', message => {
+            if (message.content === 'ÈÑÈ') {
+              message.channel.sendFile("./tyt.png");
+            }
+         });
+
+
+
+
+         client.on('message', message => {
+            if (message.content === 'Þ1') {
+              message.channel.sendFile("./5.png");
+            }
+         });
+
+
+
+
+
+         client.on('message', message => {
+            if (message.content === 'ÇáÓáÇã Úáíßã') {
+              message.channel.sendFile("./5bz.png");
+            }
+         });
+
+
+
+
+
+
+
+
+         client.on('message', message => {
+            if (message.content === 'ÍÔíÔ') {
+              message.channel.sendFile("./2.png");
+            }
+         });
+
+
+
+
+
+
+
+
+         client.on('message', message => {
+            if (message.content === 'ÈÇß') {
+              message.channel.sendFile("./WLC.png");
+            }
+         });
+         
+         
+         
+         
+         
+         
+         
+         
+         
+                 client.on('message', message => {
+            if (message.content === 'ÊÑÍíÈ') {
+              message.channel.sendFile("./1.jpg");
+            }
+         });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`heex BOT STARTED`);
+        console.log(`---------------`);
+      console.log(`ON ${client.guilds.size} Servers `);
+    console.log(`---------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`in 2 server -help`,"http://twitch.tv/y04zgamer")
+   client.user.setStatus("dnd")
+});
+
+
+
+
+
+
+
+
+ client.on('message', message => {
+     if (message.content === "bot") {
+     let embed = new Discord.RichEmbed()
+
+  .setColor("#51cde6")
+  .addField("Playing on" , client.guilds.size)
+
+
+  message.channel.sendEmbed(embed);
+
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on("message", message => {
+     if (message.content === "-kick") {
+         if(!message.channel.guild) return message.reply('** This command only for servers **');
+        message.reply("bla bla")
+     }
+});
+
+
+
+
+client.on("message", message => {
+     if (message.content === "-ban") {
+         if(!message.channel.guild) return message.reply('** This command only for servers **');
+        message.reply("bla bla")
+     }
+});
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    if (message.content.startsWith("-avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Client.on('message', message => {
+if (message.content.split(' ')[0] == '-bc')
+ message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                            message.delete();
+});
+});
+
+Client.on("message", message => {
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+ if (!args[1]) {
+                                let embed3 = new Discord.RichEmbed()
+                                .setDescription(":white_check_mark: | Êã ÇÑÓÇá ááßá ÑÓÇáå ÝÇÑÛå")
+                                .setColor("#FF0000")
+                                message.channel.sendEmbed(embed3);
+                            } else {
+
+                            let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | Êã ÇÑÓÇá ááßá ÇáÑÓÇáå !')
+                                                            .setColor("#008000")
+                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = msg.content.split(" ").slice(1);
+
+
+if (command == "ÛÑÏ") {
+    let embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setAuthor(msg.author.username, msg.author.avatarURL)
+    .setDescription(args.join(" "))
+    .setFooter('© . :AG || Copyright')
+    msg.channel.sendEmbed(embed);
+    msg.delete();
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const cuttweet = [
+     'ßÊ ÊæíÊ þ- ÊÎíøá áæ Ãäß ÓÊÑÓã ÔíÁ æÍíÏ ÝíÕÈÍ ÍÞíÞÉ¡ ãÇÐÇ ÓÊÑÓã¿',
+     'ßÊ ÊæíÊ þ- ÃßËÑ ÔíÁ íõÓßöÊ ÇáØÝá ÈÑÃíß¿',
+     'ßÊ ÊæíÊ þ- ÇáÍÑíÉ áÜ ... ¿',
+     'ßÊ ÊæíÊ þ- ÞäÇÉ ÇáßÑÊæä ÇáãÝÖáÉ Ýí ØÝæáÊß¿',
+     'ßÊ ÊæíÊ þ- ßáãÉ ááÕõÏÇÚ¿',
+     'ßÊ ÊæíÊ þ- ãÇ ÇáÔíÁ ÇáÐí íõÝÇÑÞß¿',
+     'ßÊ ÊæíÊ þ- ãÇ ÇáÔíÁ ÇáÐí íõÝÇÑÞß¿',
+     'ßÊ ÊæíÊ þ- ãæÞÝ ããíÒ ÝÚáÊå ãÚ ÔÎÕ æáÇ íÒÇá íÐßÑå áß¿',
+     'ßÊ ÊæíÊ þ- ÃíåãÇ íäÊÕÑ¡ ÇáßÈÑíÇÁ Ãã ÇáÍÈ¿',
+     'ßÊ ÊæíÊ| ÈÚÏ 10 Óäíä ÇíÔ ÈÊßæä ¿',
+
+     'ßÊ ÊæíÊ þ- ãöä ÃÛÑÈ æÃÌãá ÇáÃÓãÇÁ ÇáÊí ãÑÊ Úáíß¿',
+     'þßÊ ÊæíÊ| ÚãÑß ÔáÊ ãÕíÈÉ Úä ÔÎÕ ÈÑÛÈÊß ¿',
+'ßÊ ÊæíÊ þ- ÃßËÑ ÓÄÇá æÌöøå Åáíß ãÄÎÑðÇ¿',
+     'þßÊ ÊæíÊ|ãÇ åæ ÇáÔíÁ ÇáÐí íÌÚáß ÊÔÚÑ ÈÇáÎæÝ¿',
+     'þßÊ ÊæíÊ|æÔ íÝÓÏ ÇáÕÏÇÞÉ¿',
+     'þßÊ ÊæíÊ|ÔÎÕ áÇÊÑÝÖ áå ØáÈÇ ¿',
+     'þßÊ ÊæíÊ|ßã ãÑå ÎÓÑÊ ÔÎÕ ÊÍÈå¿.',
+     'þßÊ ÊæíÊ|ßíÝ ÊÊÚÇãá ãÚ ÇáÇÔÎÇÕ ÇáÓáÈííä ¿',
+     'þßÊ ÊæíÊ|ßáãÉ ÊÔÚÑ ÈÇáÎÌá ÇÐÇ ÞíáÊ áß¿',
+     'þßÊ ÊæíÊ|åá ÊõÎÝí äÌÇÍß Ãæ ßÊ ßÊ ÊæíÊ | åá ÊÎÝí äÌÇÍß Ãæ ÃÔíÇÆß ÇáÌãíáÉ ÎæÝÇð ãä ÇáÚíä æÇáÍÓÏ¿',
+     'þßÊ ÊæíÊ|ÌÓãß ÇßÈÑ ãä ÚñãÑß Çæ ÇáÚßÓø ¿!',
+     'þßÊ ÊæíÊ|ÃÞæì ßÐÈÉ ãÔÊ Úáíß ¿',
+     'þßÊ ÊæíÊ|ÊÊÃËÑ ÈÏãæÚ ÔÎÕ íÈßí ÞÏÇãß ÞÈá ÊÚÑÝ ÇáÓÈÈ ¿',
+     'ßÊ ÊæíÊ|åá ÍÏË æÖÍíÊ ãä ÃÌá ÔÎÕò ÃÍÈÈÊ¿',
+     'þßÊ ÊæíÊ|ÃßËÑ ÊØÈíÞ ÊÓÊÎÏãå ãÄÎÑðÇ¿',
+     'þßÊ ÊæíÊ|þÇßËÑ Ôí íÑÖíß ÇÐÇ ÒÚáÊ ÈÏæä ÊÝßíÑ ¿',
+     'þßÊ ÊæíÊ|æÔ ãÍÊÇÌ ÚÔÇä Êßæä ãÈÓæØ ¿',
+     'þßÊ ÊæíÊ|ãØáÈß ÇáæÍíÏ ÇáÍíä ¿',
+     'þßÊ ÊæíÊ|- åá ÍÏË æÔÚÑÊ ÈÃäß ÇÑÊßÈÊ ÃÍÏ ÇáÐäæÈ ÃËäÇÁ ÇáÕíÇã¿',
+]
+
+client.on('message', message => {
+  if (message.content === `-ßÊ ÊæíÊ`) {
+message.channel.sendMessage({embed: {
+  color: 3547003,
+  description: `${cuttweet[Math.floor(Math.random() * cuttweet.length)]}`
+}});
+};
+});
+
+
+
+
+
+
+
+
+
+
+client.on("guildMemberRemove", member => {
+  let guild = member.guild;
+  guild.defaultChannel.sendMessage("", {embed: {
+  color: 808080,
+  author: {
+    name: member.user.username,
+    icon_url: member.user.avatarURL
+  },
+  title: guild.name,
+  description: ' *** áÇ ÊäÓì ÊÓßÑ ÇáÈÇÈ æÑÇß :( *** ..',
+}}).catch(console.error);
+  }
+);
+client.on("guildMemberAdd", member => {
+  let guild = member.guild;
+  guild.defaultChannel.sendMessage("", {embed: {
+  color: 808080,
+  author: {
+    name: member.user.username,
+    icon_url: member.user.avatarURL
+  },
+  title: guild.name,
+  description: ' *** äæÑÊäÇ æÇááå *** !',
+}}).catch(console.error);
+  }
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+// -say
+  if (command === "say") {
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+  }
+  
+ 
+
+if (command == "embed") {
+    let say = new Discord.RichEmbed()
+    .setDescription(args.join("  "))
+    .setColor(0x23b2d6)
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+   if (message.content === "-id") {
+   let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setThumbnail(message.author.avatarURL)
+  .addField("Name:",`${message.author.username}`, true)
+  .addField('Discrim:',"#" +  message.author.discriminator, true)
+  .addField("ID:", message.author.id, true)
+  .addField("Create At:", message.author.createdAt, true)
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+client.on("message", message => {
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+ if (!args[1]) {
+                                let x5bz1 = new Discord.RichEmbed()
+                                .setDescription("-clear <number>")
+                                .setColor("#0000FF")
+                                message.channel.sendEmbed(x5bz1);
+                            } else {
+                            let messagecount = parseInt(args[1]);
+                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                                                          message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                            let x5bz2 = new Discord.RichEmbed()
+                                                            .setColor("#008000")
+                                .setDescription(":white_check_mark: | Delete " + args[1] + " Message!")
+                                                                                        message.delete("..");
+                                message.channel.sendEmbed(x5bz2);
+                            }
+                          }
+});
+
+
+
+
+
+client.on('message', message => {
+    if(message.content == prefix + 'server') {
+        var servername = message.guild.name
+        var ÇæäÑ = message.guild.owner
+        var ÇÚÖÇÁ = message.guild.memberCount
+        var ÇíÏí = message.guild.id
+        var ÈáÏÇáÓíÑÝÑ = message.guild.region
+        var ÇáÑæãÇÊ = message.guild.channels.size
+        var ÇáÑÊÈ = message.guild.roles
+        var Úãá = message.guild.createdAt
+        var ÇáÑæã = message.guild.defaultChannel
+        var server = new Discord.RichEmbed()
+        .setThumbnail(message.guild.iconURL)
+        .addField('ÇÓã ÇáÓíÑÝÑ', servername)
+        .addField('Çí Ïí ÇáÓíÑÝÑ ' , [ÇíÏí])
+        .addField('ÃÚÖÇÁ ÇáÓíÑÝÑ', ÇÚÖÇÁ)
+        .addField('ÑæãÇÊ ÇáÓíÑÝÑ', ÇáÑæãÇÊ)
+        .addField('Ñæã ÇáÔÇÊ ÇáÃÓÇÓí', ÇáÑæã)
+        .addField('ÕÇÍÈ ÇáÓíÑÝÑ', ÇæäÑ)
+        .addField('ÈáÏ ÇáÓíÑÝÑ', ÈáÏÇáÓíÑÝÑ)
+        .addField('ÊÇÑíÎ ÇÝÊÊÇÍ ÇáÓíÑÝÑ', Úãá)
+        .setColor('RANDOM')
+
+        message.channel.sendEmbed(server)
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    client.on('message', message => {
+    if(message.content.startsWith(prefix + '2avatar')) {
+         var men = message.mentions.users.first();
+      var heg;
+      if(men) {
+          heg = men
+      } else {
+          heg = message.author
+      }
+  var avatar = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle(heg.username)
+.setImage(heg.avatarURL)
+
+message.channel.sendEmbed(avatar)
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
