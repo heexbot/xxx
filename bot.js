c0c86f54030da765d6c33e7c8f5caf3d0b32dd1a
heexbot <39099888+heexbot@users.noreply.github.com>
2018-11-25 21:41:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "d";
-var adminprefix = 'البريفكس'
-const developers = ["الايدي"]
+var adminprefix = 'K'
+const developers = ["323888904602124299"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
