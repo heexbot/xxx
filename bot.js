831aa834762eda178607bdac4715e1ba75797e45
heexbot <39099888+heexbot@users.noreply.github.com>
2018-11-25 21:24:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();var prefix = "d";
+const client = new Discord.Client();
+var prefix = "d";
 var adminprefix = 'K'
 const developers=["323888904602124299"]
 client.on('message', message => {
@@ -26,6 +27,7 @@ client.user.setActivity(argresult, {type:'WATCHING'}); message.channel.send(`Las
 
 client.user.setGame(argresult, "https://www.twitch.tv/One"); message.channel.send(`LastCodes`) } if (message.content.startsWith(adminprefix + 'setname')) { client.user.setUsername(argresult).then message.channel.send(`Changing The Name To ..**${argresult}** `)} 
 
-else if (message.content.startsWith(adminprefix + 'setavatar')) { client.user.setAvatar(argresult); message.channel.send(`Changing The Avatar To :**${argresult}** `);}});
+else if (message.content.startsWith(adminprefix + 'setavatar')) {
+ client.user.setAvatar(argresult); message.channel.send(`Changing The Avatar To :**${argresult}** `);}});
 
 client.login(process.env.BOT_TOKEN);//
