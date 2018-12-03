589968db7b258e933e8a200243b742d00a578e6a
heexbot <39099888+heexbot@users.noreply.github.com>
2018-12-03 14:53:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,45 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var adminprefix = "!";
-const developers = ["459050608582983681","323888904602124299"]
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'setg')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**تم تغيير حالتك الى** **${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**تم تغيير الواتشنق الى** **${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'setl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**تم تغير اللستنق الى ** **${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'sets')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/dream");
-      message.channel.send(`**تم تغيير الستريمنق الى** **${argresult}**`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`**تم تغيير اسمك الى ** ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setava')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`**تم تغيير صورتك الى**:**${argresult}** `);
-}
+
+
+client.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("500838241784823814");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`*WELCOME TO DESERT*`), 4000)        
+
 });
 
 
-
-
-
-
-
-
-client.login(process.env.BOT_TOKEN);
+cient.login(process.env.BOT_TOKEN);
